a1c9dad6f8edfbf84e6bf61ffbb2e0b68f86508e
prabualwan <fharlup@gmail.com>
2024-06-15 16:00:22 +0700

    tambah fungsi

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,42 @@
+
+
+
+
+using System;
+using System.IO;
+using System.Linq;
+using Bengkel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestTambahMotor()
         {
+            // Arrange
+            var bengkel = new BengkelState();
+            string input = "2\nMotor1\nMotor2\n";
+            var stringReader = new StringReader(input);
+            Console.SetIn(stringReader);
+
+            // Act
+            bengkel.TambahMotor();
+
+            // Assert
+            Assert.AreEqual(2, bengkel.MotorList.Count);
+            Assert.IsTrue(bengkel.MotorList.ContainsKey("Motor1"));
+            Assert.IsTrue(bengkel.MotorList.ContainsKey("Motor2"));
+            Assert.AreEqual(StatusPerbaikan.Ada, bengkel.MotorList["Motor1"]);
+            Assert.AreEqual(StatusPerbaikan.Ada, bengkel.MotorList["Motor2"]);
         }
     }
 }
+
+
+
+
+
+
